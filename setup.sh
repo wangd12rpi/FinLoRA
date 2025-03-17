8f2bc6d40e91d9dcac4bd39a929d81a2f8afdc84
Dannong Wang <wand12@rpi.edu>
2025-03-17 18:00:25 -0400

    change setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 export HF_TOKEN=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
 pip install transformers datasets bitsandbytes peft scikit-learn tqdm tensorboard wandb deepspeed
 pip install cupy-cuda12x
-pip install --upgrade torch
 #git clone https://github.com/YangletLiu/FinLoRA
-pip install typing_extensions==4.7.1 --upgrade
+pip install typing_extensions==4.14.0 --upgrade
