380d122ce5dc008c81417c036bd181b474eeabb2
Dannong Wang <wand12@rpi.edu>
2025-04-21 01:31:51 -0400

    new documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -45,7 +45,7 @@ language = 'en'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "furo"
+html_theme = "sphinx-rtd-theme"
 
 html_static_path = ['_static']
 html_css_files = [
